32234e0487807220267dc8f678f326065c3974b1
istarlet <istarlet@gmail.com>
2022-09-07 14:10:51 -0400

    Add javascript

---
+++ main.js
@@ -0,0 +1,8 @@
+const homeBtn1 = document.getElementById("home-btn-1")
+const homeBtn2 = document.getElementById("home-btn-2")
+const homeBtn3 = document.getElementById("home-btn-3")
+
+const guestBtn1 = document.getElementById("guest-btn-1")
+const guestBtn2 = document.getElementById("guest-btn-2")
+const guestBtn3 = document.getElementById("guest-btn-3")
+
